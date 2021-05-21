11b04a7c933d00599b42022cf49bb19d24418057
Ple <plegamemaster@hotmail.com>
2021-05-21 19:11:47 +0700

    ###First commit

---
+++ ExampleStack/MyStack.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetStack
+{
+    public class MyStack<T>
+    {
+        private int _size;
+        private LinkedList<T> _sList;
+
+        public MyStack(int size)
+        {
+            _size = size;
+            _sList = new LinkedList<T>();
+        }
+
+        #region Stack Class
+
+        /// <summary>
+        /// Add element into the stack.
+        /// </summary>
+        /// <param name="vale"></param>
+        public void Push(T vale)
+        {
+            if (_sList.Count == _size) { _sList.RemoveLast(); }
+            _sList.AddFirst(vale);
+        }
+
+        /// <summary>
+        /// Removes and returns the object at the top of the System.Collections.Stack.
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            try
+            {
+                if (_sList.Count > 0)
+                {
+                    _sList.RemoveFirst();
+                    return _sList.First.Value; ;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Stack is empty.");
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+        }
+
+        /// <summary>
+        /// Returns the object at the top of the System.Collections.Stack without removing
+        /// </summary>
+        /// <returns></returns>
+        public T Peek()
+        {
+            if (_sList.Count > 0)
+            {
+                return _sList.First.Value;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+        }
+
+        /// <summary>
+        /// Return the total element of stack.
+        /// </summary>
+        public int Count { get { return this._sList.Count; } }
+
+        /// <summary>
+        /// Check if element is to of stack.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsTop(T value)
+        {
+            if (_sList.Count > 0)
+            {
+                return Peek().Equals(value);
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+        }
+
+        /// <summary>
+        /// Search element if is member of stack.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Contains(T value)
+        {
+            if (_sList.Count > 0)
+            {
+                bool isMatch = _sList.Contains(value);
+                return _sList.Contains(value);
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+        }
+
+        /// <summary>
+        /// Clear stack.
+        /// </summary>
+        public void Clear()
+        {
+            this._sList.Clear();
+        }
+
+        #endregion
+
+        public IEnumerator GetEnumerator()
+        {
+            return _sList.GetEnumerator();
+        }
+
+    }
+}
---
+++ ExampleStack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace DotNetStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<string> mStack = new MyStack<string>(5);
+
+            #region Create
+            Console.WriteLine("----- Create Stack -----");
+            mStack.Push("XRP");
+            mStack.Push("DOGE");
+            mStack.Push("BNB");
+            mStack.Push("ADA");
+            mStack.Push("KUB");
+
+            Console.WriteLine($"Total element of stack --> {mStack.Count}");
+            Console.WriteLine("Read stack..");
+            int counter = 0;
+            foreach (var ele in mStack)
+            {
+                counter++;
+                Console.WriteLine($"{counter}-->{ele}");
+            }
+
+            Console.WriteLine($"Get thr top of stack without remove --> {mStack.Peek()}");
+            Console.WriteLine($"Get the top of stack after removed --> {mStack.Pop()}");
+            Console.WriteLine($"Current total element --> {mStack.Count}");
+            #endregion
+
+            #region Search
+            Console.WriteLine("----- Search -----");
+            Console.WriteLine($"Searching ETH in stack...the result is {(mStack.Contains("ETH") == true ? $"found." : $"not found.")}");
+            Console.WriteLine($"Searching BNB in stack...the result is {(mStack.Contains("BNB") == true ? $"found." : $"not found.")}");
+            Console.WriteLine($"Searching XRP in stack...the result is {(mStack.Contains("XRP") == true ? $"found." : $"not found.")}");
+            Console.WriteLine($"Searching CAKE in stack...the result is {(mStack.Contains("CAKE") == true ? $"found." : $"not found.")}");
+            #endregion
+
+            #region Search with input
+            //string searchIput = Console.ReadLine();
+            //Console.WriteLine($"Searching ETH in stack...the result is {(mStack.Contains(searchIput) == true ? $"found {searchIput}." : $"{searchIput} is not found.")}");
+            #endregion
+
+            #region Clear Stack
+            Console.WriteLine("----- Clear Stack -----");
+            mStack.Clear();
+            Console.WriteLine($"Totle element of stack after clear --> {mStack.Count}");
+            #endregion
+
+            Console.ReadLine();
+        }
+
+    }
+}
